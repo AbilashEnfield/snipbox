afa5f85091584325f8e0557dc9b32c03712656ef
abilash <abhilashk@sparksupport.com>
2025-07-24 11:39:46 +0530

    added snippet create api

--- snipbox/snipbox/urls.py
+++ snipbox/snipbox/urls.py
@@ -21,9 +21,9 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include('snippets.urls')),
 ]
--- snipbox/snippets/views.py
+++ snipbox/snippets/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from .models import Snippet, Tag
+from .serializers import SnippetSerializer
 
-# Create your views here.
+
+class Snippet(viewsets.ViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request):
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
