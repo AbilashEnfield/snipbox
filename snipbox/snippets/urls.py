069015ea0c0f47043ab9488ed7d2d27d147b3b42
abilash <abhilashk@sparksupport.com>
2025-07-24 12:07:59 +0530

    added tags details api

--- snipbox/snippets/urls.py
+++ snipbox/snippets/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Snippet, TagList
+from .views import Snippet, TagList, TagDetails
 
 
 urlpatterns = [
     path('snippets/', Snippet.as_view({'post': 'create', 'get': 'list'}), name='snippet'),
     path('snippets/<int:pk>/', Snippet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='snippet_detail'),
-    
+    path('tags/', TagList.as_view(), name='tag'),
+    path('tags/<int:pk>/', TagDetails.as_view(), name='tag_detail'),
 ]
